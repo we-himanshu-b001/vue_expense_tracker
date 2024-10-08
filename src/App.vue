7f1b07ab25806947fc6cdc5a935b31e23d9b8391
Himanshu Bamania <himanshu-b001@webreinvent.com>
2024-10-08 16:26:18 +0530

    initial project files

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance :total="+total"></Balance>
+<!--    <IncomeExpense></IncomeExpense>-->
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"></TransactionList>
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import { ref, computed } from 'vue';
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
+
+const transactions=ref([
+  {id:1,name:'Flower',amount:-9.99},
+  {id:2,name:'Salary',amount:1119.99},
+  {id:3,name:'Book',amount:29.99},
+  {id:4,name:'Camera',amount:99.99},
+]);
+
+const total=computed(()=>{
+  return transactions.value.reduce((acc,transaction) => {
+    return acc+transaction.amount;
+  },0);
+});
+
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: Math.floor(Math.random()*100000),
+    name:transactionData.text,
+    amount:transactionData.amount,
+  });
+
+  toast.success('Transaction Added');
+};
+
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+      (transaction) => transaction.id !== id
+  );
+  toast.success('Transaction Deleted');
+}
+
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li v-for="t in transactions" :key="t.id" :class="t.amount <0 ? 'minus':'plus'">
+      {{t.name}} <span>${{t.amount}}</span><button class="delete-btn" @click="deleteTransaction(t.id)">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const emit = defineEmits(['transactionDeleted']);
+
+  const props = defineProps({
+    transactions:{
+      type:Array,
+      required: true
+    }
+  })
+
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted',id);
+  }
+</script>
